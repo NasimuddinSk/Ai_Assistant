16ae849f4a6abc55e47c6cadf7ef34185a43fb6a
Nasimuddin Sk <snasim251@email.com>
2023-12-10 21:09:52 +0530

    Splash Screen Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
+import 'package:ai_assistant/screen/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() {
+Future<void> main() async {
+  // For Full Screen
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+  // For Urientions
+  await SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
   runApp(const MyApp());
 }
 
@@ -11,11 +20,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Text("Hello World!!"),
-        ),
-      ),
+      home: SplashScreen(),
     );
   }
 }
