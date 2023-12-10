6563a475545d4e209cb7e24db88ad7cb580972f3
Nasimuddin Sk <snasim251@email.com>
2023-12-10 21:46:11 +0530

    added lottie animation

--- lib/screen/splash_screen.dart
+++ lib/screen/splash_screen.dart
@@ -1,5 +1,6 @@
 import 'package:ai_assistant/helper/global.dart';
 import 'package:ai_assistant/screen/home_screen.dart';
+import 'package:ai_assistant/widget/custom_loading.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -24,20 +25,32 @@ class _SplashScreenState extends State<SplashScreen> {
     // Initilizing device size
     mq = MediaQuery.sizeOf(context);
     return Scaffold(
-        body: Center(
-      child: Card(
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(
-            Radius.circular(20),
+        body: SizedBox(
+      width: double.maxFinite,
+      child: Column(
+        children: [
+          const Spacer(
+            flex: 2,
           ),
-        ),
-        child: Padding(
-          padding: EdgeInsets.all(mq.width * .05),
-          child: Image.asset(
-            "assets/images/logo.png",
-            width: mq.width * .4,
+          Card(
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(20),
+              ),
+            ),
+            child: Padding(
+              padding: EdgeInsets.all(mq.width * .05),
+              child: Image.asset(
+                "assets/images/logo.png",
+                width: mq.width * .4,
+              ),
+            ),
           ),
-        ),
+          // spacer
+          const Spacer(),
+          const CustomLoading(),
+          const Spacer(),
+        ],
       ),
     ));
   }
